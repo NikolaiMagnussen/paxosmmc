d7bdab9d0ddf955d57b9e7e61fa0e27bdbaa1dc4
Nikolai Magnussen <nikolai@magnussen.tf>
2018-02-20 12:07:44 +0100

    Translate from python2 to python3, and inserted some prints for debugging

--- code/initial/env.py
+++ code/initial/env.py
@@ -1,10 +1,12 @@
-import os, signal, sys, time
+import signal
+import sys
+import time
 from acceptor import Acceptor
 from leader import Leader
 from message import RequestMessage
-from process import Process
 from replica import Replica
-from utils import *
+from timing import Timing
+from utils import Config, Command, ReconfigCommand, WINDOW
 
 NACCEPTORS = 3
 NREPLICAS = 2
@@ -12,89 +14,104 @@ NLEADERS = 2
 NREQUESTS = 10
 NCONFIGS = 2
 
+
 class Env:
-  def __init__(self):
-    self.procs = {}
+    def __init__(self, max_clients=NREQUESTS):
+        self.max_clients = max_clients
+        self.procs = {}
+        print(("Max clients: {}".format(self.max_clients)))
 
-  def sendMessage(self, dst, msg):
-    if dst in self.procs:
-      self.procs[dst].deliver(msg)
+    def sendMessage(self, dst, msg):
+        if dst in self.procs:
+            self.procs[dst].deliver(msg)
 
-  def addProc(self, proc):
-    self.procs[proc.id] = proc
-    proc.start()
+    def addProc(self, proc):
+        self.procs[proc.id] = proc
+        proc.start()
 
-  def removeProc(self, pid):
-    del self.procs[pid]
+    def removeProc(self, pid):
+        del self.procs[pid]
 
-  def run(self):
-    initialconfig = Config([], [], [])
-    c = 0
+    def run(self):
+        initialconfig = Config([], [], [])
+        c = 0
 
-    for i in range(NREPLICAS):
-      pid = "replica %d" % i
-      Replica(self, pid, initialconfig)
-      initialconfig.replicas.append(pid)
-    for i in range(NACCEPTORS):
-      pid = "acceptor %d.%d" % (c,i)
-      Acceptor(self, pid)
-      initialconfig.acceptors.append(pid)
-    for i in range(NLEADERS):
-      pid = "leader %d.%d" % (c,i)
-      Leader(self, pid, initialconfig)
-      initialconfig.leaders.append(pid)
-    for i in range(NREQUESTS):
-      pid = "client %d.%d" % (c,i)
-      for r in initialconfig.replicas:
-        cmd = Command(pid,0,"operation %d.%d" % (c,i))
-        self.sendMessage(r,RequestMessage(pid,cmd))
-        time.sleep(1)
+        for i in range(NREPLICAS):
+            pid = "replica %d" % i
+            Replica(self, pid, initialconfig)
+            initialconfig.replicas.append(pid)
+        for i in range(NACCEPTORS):
+            pid = "acceptor %d.%d" % (c, i)
+            Acceptor(self, pid)
+            initialconfig.acceptors.append(pid)
+        for i in range(NLEADERS):
+            pid = "leader %d.%d" % (c, i)
+            Leader(self, pid, initialconfig)
+            initialconfig.leaders.append(pid)
+        for i in range(self.max_clients):
+            pid = "client %d.%d" % (c, i)
+            for r in initialconfig.replicas:
+                cmd = Command(pid, 0, "operation %d.%d" % (c, i))
+                self.sendMessage(r, RequestMessage(pid, cmd))
+                time.sleep(1)
 
-    for c in range(1, NCONFIGS):
-      # Create new configuration
-      config = Config(initialconfig.replicas, [], [])
-      for i in range(NACCEPTORS):
-        pid = "acceptor %d.%d" % (c,i)
-        Acceptor(self, pid)
-        config.acceptors.append(pid)
-      for i in range(NLEADERS):
-        pid = "leader %d.%d" % (c,i)
-        Leader(self, pid, config)
-        config.leaders.append(pid)
-      # Send reconfiguration request
-      for r in config.replicas:
-        pid = "master %d.%d" % (c,i)
-        cmd = ReconfigCommand(pid,0,str(config))
-        self.sendMessage(r, RequestMessage(pid, cmd))
-        time.sleep(1)
-      for i in range(WINDOW-1):
-        pid = "master %d.%d" % (c,i)
-        for r in config.replicas:
-          cmd = Command(pid,0,"operation noop")
-          self.sendMessage(r, RequestMessage(pid, cmd))
-          time.sleep(1)
-      for i in range(NREQUESTS):
-        pid = "client %d.%d" % (c,i)
-        for r in config.replicas:
-          cmd = Command(pid,0,"operation %d.%d"%(c,i))
-          self.sendMessage(r, RequestMessage(pid, cmd))
-          time.sleep(1)
+        for c in range(1, NCONFIGS):
+            print(("\nConfig: {}".format(c)))
+            # Create new configuration
+            config = Config(initialconfig.replicas, [], [])
+            for i in range(NACCEPTORS):
+                pid = "acceptor %d.%d" % (c, i)
+                Acceptor(self, pid)
+                config.acceptors.append(pid)
+            for i in range(NLEADERS):
+                pid = "leader %d.%d" % (c, i)
+                Leader(self, pid, config)
+                config.leaders.append(pid)
+            # Send reconfiguration request
+            for r in config.replicas:
+                pid = "master %d.%d" % (c, i)
+                cmd = ReconfigCommand(pid, 0, str(config))
+                self.sendMessage(r, RequestMessage(pid, cmd))
+                time.sleep(1)
+            for i in range(WINDOW-1):
+                pid = "master %d.%d" % (c, i)
+                for r in config.replicas:
+                    cmd = Command(pid, 0, "operation noop")
+                    self.sendMessage(r, RequestMessage(pid, cmd))
+                    time.sleep(1)
+            for i in range(self.max_clients):
+                pid = "client %d.%d" % (c, i)
+                print(pid)
+                for r in config.replicas:
+                    cmd = Command(pid, 0, "operation %d.%d" % (c, i))
+                    self.sendMessage(r, RequestMessage(pid, cmd))
+                    time.sleep(1)
+            print("Shits")
 
-  def terminate_handler(self, signal, frame):
-    self._graceexit()
+    def terminate_handler(self, signal, frame):
+        self._graceexit()
 
-  def _graceexit(self, exitcode=0):
-    sys.stdout.flush()
-    sys.stderr.flush()
-    os._exit(exitcode)
-
-def main():
-  e = Env()
-  e.run()
-  signal.signal(signal.SIGINT, e.terminate_handler)
-  signal.signal(signal.SIGTERM, e.terminate_handler)
-  signal.pause()
+    def _graceexit(self, exitcode=0):
+        sys.stdout.flush()
+        sys.stderr.flush()
+        return
 
 
-if __name__=='__main__':
-  main()
+def main(max_clients):
+    e = Env(max_clients)
+    with Timing("kake"):
+        e.run()
+        signal.signal(signal.SIGINT, e.terminate_handler)
+        signal.signal(signal.SIGTERM, e.terminate_handler)
+        print("Waiting for processes to finish")
+        signal.pause()
+    print("\nJeg er ferdig!\n")
+
+
+if __name__ == '__main__':
+    print(("{}".format(sys.argv)))
+    if len(sys.argv) < 2:
+        print(("{} size=<size>".format(sys.argv[0])))
+        sys.exit(0)
+    else:
+        main(int(sys.argv[1].split("=")[1]))
--- code/initial/utils.py
+++ code/initial/utils.py
@@ -1,45 +1,48 @@
 from collections import namedtuple
 
 WINDOW = 5
+NUM_ACCEPTED_PROPOSALS = 0
 
-class BallotNumber(namedtuple('BallotNumber',['round','leader_id'])):
-  __slots__ = ()
-  def __str__(self):
-    return "BN(%d,%s)" % (self.round, str(self.leader_id))
 
-class PValue(namedtuple('PValue',['ballot_number',
-                                  'slot_number',
-                                  'command'])):
-  __slots__ = ()
-  def __str__(self):
-    return "PV(%s,%s,%s)" % (str(self.ballot_number),
-                             str(self.slot_number),
-                             str(self.command))
+class BallotNumber(namedtuple('BallotNumber', ['round', 'leader_id'])):
+    __slots__ = ()
 
-class Command(namedtuple('Command',['client',
-                                    'req_id',
-                                    'op'])):
-  __slots__ = ()
-  def __str__(self):
-    return "Command(%s,%s,%s)" % (str(self.client),
-                                  str(self.req_id),
-                                  str(self.op))
+    def __str__(self):
+        return f"BN({self.round},{self.leader_id})"
 
-class ReconfigCommand(namedtuple('ReconfigCommand',['client',
-                                                    'req_id',
-                                                    'config'])):
-  __slots__ = ()
-  def __str__(self):
-    return "ReconfigCommand(%s,%s,%s)" % (str(self.client),
-                                          str(self.req_id),
-                                          str(self.config))
 
-class Config(namedtuple('Config',['replicas',
-                                  'acceptors',
-                                  'leaders'])):
-  __slots__ = ()
-  def __str__(self):
-    return "%s;%s;%s" % (','.join(self.replicas),
-                         ','.join(self.acceptors),
-                         ','.join(self.leaders))
+class PValue(namedtuple('PValue', ['ballot_number',
+                                   'slot_number',
+                                   'command'])):
+    __slots__ = ()
 
+    def __str__(self):
+        return f"PV({self.ballot_number},{self.slot_number},{self.command})"
+
+
+class Command(namedtuple('Command', ['client',
+                                     'req_id',
+                                     'op'])):
+    __slots__ = ()
+
+    def __str__(self):
+        return f"Command({self.client},{self.req_id},{self.op})"
+
+
+class ReconfigCommand(namedtuple('ReconfigCommand', ['client',
+                                                     'req_id',
+                                                     'config'])):
+    __slots__ = ()
+
+    def __str__(self):
+        return f"ReconfigCommand({self.client},{self.req_id},{self.config})"
+
+
+class Config(namedtuple('Config', ['replicas',
+                                   'acceptors',
+                                   'leaders'])):
+    __slots__ = ()
+
+    def __str__(self):
+        return f"{','.join(self.replicas)},{','.join(self.acceptors)},\
+                 {','.join(self.leaders)}"
